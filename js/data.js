7f883ad0d605ce736fbe59d9569956c58d7e4f98
gurutze-fagoaga <gurutzefagoaga@alumnos.cei.es>
2025-06-05 19:10:37 +0200

    proyectos actualizados

--- js/data.js
+++ js/data.js
@@ -1,24 +1,89 @@
 const projects = {
   plaza: {
+    id: "plaza",
   title: "Plaza",
   tags: ["diseño web", "HTML", "CSS"],
   descriptionLeft:"Plaza es un espacio creativo (ficticio) situado en Pamplona (Navarra), que imparte talleres creativos, y ofrece un espacio de coworking para creativo/as de diversas disciplinas artísticas. Además, organiza eventos mensuales con objetivo de ser un punto de encuentro entre creadores y clientes.",
   descriptionRight: "En este proyecto he creado la identidad de marca de Plaza, y he llevado a cabo el diseño web con HTML, CSS y Javascript.",
-  namingTitle: "Naming",
+  namingTitle: "Contexto",
   naming: `
           La plaza ha sido históricamente el punto de encuentro, el espacio social, el centro del pueblo. 
           
-          Plaza es un espacio creativo que invita a acercarse, relacionarse y abrisrse al pueblo y a su gente. Es un espacio abierto a crear y compartir.
+          Plaza es un espacio creativo que invita a acercarse, relacionarse y abrisrse al pueblo y a su gente. Es un espacio abierto a crear y compartir. 
   `,
-  styleTitle: "Colores y tipografía",
-  style: `
-    Los cuatro colores principales diferencian cada apartado...
-    Tipografía principal: Kanit regular<br>
-    Tipografía secundaria: Lato regular `,
+  styleTitle: "Decisiones de diseño",
+  style: `La identidad visual de la marca está diseñada para conectar con un público joven y creativo. Se ha optado por una gama cromática colorida y vibrante que transmite dinamismo, frescura y energía. Además, se han elegido tipografías modernas, versátiles y con un toque informal que refuerzan una personalidad cercana y desenfadada.`,
   
-  imagesTop: ["plaza-1.jpg", "plaza-2.jpg"],
-  imagesBottom: ["plaza-3.jpg"]
+  imagesTop: ["plaza03.jpg","plaza01.jpg"],
+  imagesBottom: ["plaza02.jpg"]
+
+},
+
+  bilbao: {
+    id: "bilbao",
+  title: "Identidad visual de Bilbao",
+  tags: ["identidad visual"],
+  descriptionLeft:"Este proyecto aborda la identidad visual de Bilbao partiendo de cinco conceptos: tradición, modernidad, arte, gastronomía e historia. Para realizar la conexión de dichos conceptos, partimos de cinco puentes más caracterísiticos de Bilbao, que se conectan con las ideas ya sea por su ubicación, historia o modernidad.",
+  descriptionRight:"",
+  namingTitle: "Contexto",
+  naming: `
+         Para aplicar el proyecto a la realidad, hemos creado un recorrido por el paseo de la Ría, pasando por los puentes mencionados, y también por los soportes físicos con forma de arco que aluden a los puentes.
+  `,
+  styleTitle: "Decisiones de diseño",
+  style: `
+    Para aplicar el proyecto a la realidad, hemos creado un recorrido por el paseo de la Ría, pasando por los puentes mencionados, y también por los soportes físicos con forma de arco que aluden a los puentes.`,
+  
+  imagesTop: ["bilbao01.jpg", "bilbao02.jpg"],
+  imagesBottom: ["bilbao03.jpg", "bilbao04.jpg", "bilbao05.jpg", "bilbao06.jpg"]
+
+},
+
+  secreto: {
+    id: "secreto",
+  title: "Secreto",
+  tags: ["identidad visual", "packaging"],
+  descriptionLeft:"Creación, conceptualización y diseño del packaging de la marca de chocolates Secreto.",
+  descriptionRight:"“El secreto está guardado bajo llave. Está encerrado tras una antigua cerradura dorada. Pero si miras por el ojo de la cerradura se puede descubrir. La antigua llave dorada se encuantra en manos de todo aquel que quiera abrir la puerta.“",
+  namingTitle: "",
+  naming: "",
+  styleTitle: "",
+  style:"",
+  imagesTop: ["secreto00.jpg", "secreto03.jpg"],
+  imagesBottom: ["secreto01.jpg","secreto02.jpg","secreto04.jpg"]
+
+},
+
+  vela: {
+    id: "vela",
+  title: "Vela",
+  tags: ["identidad visual", "packaging"],
+  descriptionLeft:"Identidad visual y verbal y diseño de packaging de la marca de café Vela.",
+  descriptionRight:"Vela propone cuatro tipos de café, segun cuatro tipos de consumidores del producto: “Excusa”, “adicta”, “caprichosa” y “refinada”.  Mediante la identidad verbal (pequeñas narraciones sobre el tipo del consumidor) y la identidad visual (ilustraciones) se crea una marca única y con una historia que contar.",
+  namingTitle: "",
+  naming: "",
+  styleTitle: "",
+  style:"",
+  imagesTop: [ "vela04.jpg", "vela01.jpg"],
+  imagesBottom: ["vela02.jpg", "vela05.jpg", "vela06.jpg", "vela07.jpg", "vela08.jpg"]
+
+},
+
+  carlton: {
+    id: "carlton",
+  title: "Catálogo del Hotel Carlton",
+  tags: ["identidad visual", "packaging"],
+  descriptionLeft:"Propuesta de rediseño del catálogo físico del Hotel Carlton de Bilbao.",
+  descriptionRight:"",
+  namingTitle: "",
+  naming: "",
+  styleTitle: "",
+  style:"",
+  imagesTop: [],
+  imagesBottom: ["carlton01.jpg", "carlton00.jpg", "carlton02.jpg", "carlton03.jpg", "carlton04.jpg", "carlton05.jpg"]
 
 }
+
+
+
   // Puedes agregar más proyectos aquí
 };
--- js/proyecto.js
+++ js/proyecto.js
@@ -13,9 +13,6 @@ if (project) {
 document.getElementById("project-description-right").textContent = project.descriptionRight;
 document.getElementById("title-naming").textContent = project.namingTitle;
 document.getElementById("title-style").textContent = project.styleTitle;
-
-  document.getElementById("project-link").href = project.link;
-  document.getElementById("project-link").textContent = "Visita la web de " + project.title;
   document.getElementById("project-naming").textContent = project.naming;
   document.getElementById("project-style").textContent = project.style;
 
@@ -42,3 +39,26 @@ function renderImages(imageList, containerId, altText) {
 renderImages(project.imagesTop, "project-images-top", project.title);
 renderImages(project.imagesBottom, "project-images-bottom", project.title);
 };
+
+
+
+/* BOTON SIGUIENTE */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const currentId = urlParams.get("id");
+
+  const btnSiguiente = document.getElementById("siguienteProyecto");
+
+  btnSiguiente.addEventListener("click", () => {
+    // Buscar la posición del proyecto actual en el array
+    const index = projects.findIndex(proy => proy.id === currentId);
+
+    // Calcular el índice del siguiente (cíclico)
+    const siguienteIndex = (index + 1) % projects.length;
+    const siguienteId = proyects[siguienteIndex].id;
+
+    // Redirigir
+    window.location.href = `proyecto.html?id=${siguienteId}`;
+  });
+});
